13ad4f5f84542ec682292a0461e274f13c426f74
Ant89ne <antoine.bralet@cpe.fr>
2021-02-25 20:14:01 +0100

    .

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ conf = ReadFiles(0)
 output = Output(0)
 
 for k in range(conf.intersectionNumber) :
-    s = getStreets(k)
+    s = conf.getStreets(k)
 
     output.setIntersection(k, s, [1 for i in range(len(s))])
 

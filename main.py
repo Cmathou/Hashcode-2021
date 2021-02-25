c133c8f85956da8caba244433184f8506865c585
Ant89ne <antoine.bralet@cpe.fr>
2021-02-25 20:55:10 +0100

    .

--- main.py
+++ main.py
@@ -4,15 +4,11 @@ from utils import *
 
 conf = ReadFiles(0)
 output = Output(0)
+tmin, conf = computeMinTime(conf)
+for k in range(conf.intersectionsNumber) :
+    s = conf.getStreets(k)
 
-print(computeMinTime(conf))
-print(conf.duration)
-
-
-# for k in range(conf.intersectionsNumber) :
-#     s = conf.getStreets(k)
-
-#     output.setIntersection(k, s, [1 for i in range(len(s))])
+    output.setIntersection(k, s, [conf.streets[i][3] for i in s])
 
 
 

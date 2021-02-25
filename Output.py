037dae21302873b10300ab189cd3718297e0e953
Cmathou <cedric_mathou@hotmail.fr>
2021-02-25 19:45:10 +0100

    output

--- Output.py
+++ Output.py
@@ -3,14 +3,16 @@ class Output:
     _file = ""
 
     def __init__(self, file):
-        self._file = file
+        self._file = "output/" + file
         with open(self._file, "w+") as file:
-            file.writelines('0')
+            file.write('0\n')
 
     def setIntersection(self, intersection, streets, time):
-        with open(self._file, "w+") as file:
+        with open(self._file, "r") as file:
             content = file.readlines()
-            content[0] = str(int(content[0]) + 1)
+
+        with open(self._file, "w") as file:
+            content[0] = str(int(content[0]) + 1) + '\n'
             for i in range(len(streets)):
                 content.append(intersection + '\n')
                 content.append(streets[i] + ' ' + time[i] + '\n')
